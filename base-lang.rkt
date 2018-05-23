5e4aa42288ff474a7541e8dc783eaada488a0bac
Andrew Kent <pnwamk@gmail.com>
2018-05-23 16:42:00 -0400

    initial commit

---
+++ base-lang.rkt
@@ -0,0 +1,207 @@
+#lang racket/base
+
+(require redex/reduction-semantics
+         racket/match
+         racket/list)
+
+(provide (all-defined-out))
+
+
+(define-language base
+  [nbase ::=
+         Zero
+         One
+         Byte-Larger-Than-One
+         Positive-Index-Not-Byte
+         Positive-Fixnum-Not-Index
+         Negative-Fixnum
+         Positive-Integer-Not-Fixnum
+         Negative-Integer-Not-Fixnum
+         Positive-Rational-Not-Integer
+         Negative-Rational-Not-Integer
+         Float-NaN
+         Float-Positive-Zero
+         Float-Negative-Zero
+         Positive-Float-No-NaN
+         Negative-Float-No-NaN
+         Single-Float-NaN
+         Single-Float-Positive-Zero
+         Single-Float-Negative-Zero
+         Positive-Single-Float-No-NaN
+         Negative-Single-Float-No-NaN
+         Exact-Imaginary
+         Exact-Complex
+         Float-Imaginary
+         Single-Float-Imaginary
+         Float-Complex
+         Single-Float-Complex]
+  [ntype ::= nbase (Or nbase ...)]
+  [val ::= number]
+  [uop ::= add1]
+  [bop ::= +]
+  [comp ::= <]
+  [pred ::= number? positive?]
+  [nexp ::= val (uop nexp) (bop nexp nexp)]
+  [bexp ::= (pred nexp) (comp nexp nexp)])
+
+(define-metafunction base
+  union : ntype ... -> ntype
+  [(union nbase ...)
+   ,(match (remove-duplicates (term (nbase ...)))
+      [(list t) t]
+      [ts (cons 'Or ts)])]
+  [(union any_0 ... (Or nbase ...) any_1 ...)
+   (union any_0 ... nbase ... any_1 ...)])
+
+(define ntype? (redex-match? base ntype))
+
+(define-syntax-rule (define-numeric-unions [name (content contents ...)] ...)
+  (begin
+    (define-term name (union content contents ...))
+    ...
+    (unless (ntype? (term name))
+      (error 'define-numeric-unions "invalid ntype ~a" (term name)))
+    ...))
+
+(define-numeric-unions
+  [Positive-Byte (One Byte-Larger-Than-One)]
+  [Byte (Zero Positive-Byte)]
+  
+  [Positive-Index (One Byte-Larger-Than-One Positive-Index-Not-Byte)]
+  [Index (Zero Positive-Index)]
+  
+  [Positive-Fixnum (Positive-Fixnum-Not-Index Positive-Index)]
+  [Nonnegative-Fixnum (Positive-Fixnum Zero)]
+  
+  [Nonpositive-Fixnum (Negative-Fixnum Zero)]
+  [Fixnum (Negative-Fixnum Zero Positive-Fixnum)]
+
+  [Positive-Integer (Positive-Integer-Not-Fixnum Positive-Fixnum)]
+  [Nonnegative-Integer (Zero Positive-Integer)]
+
+  [Negative-Integer (Negative-Fixnum Negative-Integer-Not-Fixnum)]
+  [Nonpositive-Integer (Negative-Integer Zero)]
+  [Integer (Negative-Integer Zero Positive-Integer)]
+
+  [Positive-Rational (Positive-Rational-Not-Integer Positive-Integer)]
+  [Nonnegative-Rational (Zero Positive-Rational)]
+
+  [Negative-Rational (Negative-Rational-Not-Integer Negative-Integer)]
+  [Nonpositive-Rational (Negative-Rational Zero)]
+  [Rational (Negative-Rational Zero Positive-Rational)]
+
+  [Float-Zero (Float-Positive-Zero Float-Negative-Zero Float-NaN)]
+  [Positive-Float (Positive-Float-No-NaN Float-NaN)]
+  [Nonnegative-Float (Positive-Float Float-Zero)]
+  [Negative-Float (Negative-Float-No-NaN Float-NaN)]
+  [Nonpositive-Float (Negative-Float Float-Zero)]
+  [Float (Negative-Float-No-NaN Float-Negative-Zero Float-Positive-Zero
+                                Positive-Float-No-NaN Float-NaN)]
+
+  [Single-Float-Zero (Single-Float-Positive-Zero Single-Float-Negative-Zero Single-Float-NaN)]
+
+  [Inexact-Real-NaN (Float-NaN Single-Float-NaN)]
+  [Inexact-Real-Positive-Zero (Single-Float-Positive-Zero Float-Positive-Zero)]
+  [Inexact-Real-Negative-Zero (Single-Float-Negative-Zero Float-Negative-Zero)]
+  [Inexact-Real-Zero (Inexact-Real-Positive-Zero
+                      Inexact-Real-Negative-Zero
+                      Inexact-Real-NaN)]
+
+  [Positive-Single-Float (Positive-Single-Float-No-NaN Single-Float-NaN)]
+  [Positive-Inexact-Real (Positive-Single-Float Positive-Float)]
+  [Nonnegative-Single-Float (Positive-Single-Float Single-Float-Zero)]
+  [Nonnegative-Inexact-Real (Positive-Inexact-Real Inexact-Real-Zero)]
+  [Negative-Single-Float (Negative-Single-Float-No-NaN Single-Float-NaN)]
+  [Negative-Inexact-Real (Negative-Single-Float Negative-Float)]
+  [Nonpositive-Single-Float (Negative-Single-Float Single-Float-Zero)]
+  [Nonpositive-Inexact-Real (Negative-Inexact-Real Inexact-Real-Zero)]
+  [Single-Float (Negative-Single-Float
+                 Single-Float-Negative-Zero
+                 Single-Float-Positive-Zero
+                 Positive-Single-Float
+                 Single-Float-NaN)]
+  [Inexact-Real (Single-Float Float)]
+
+  [Real-Zero (Zero Inexact-Real-Zero)]
+  [Real-Zero-No-NaN (Zero Inexact-Real-Positive-Zero Inexact-Real-Negative-Zero)]
+  [PosReal (Positive-Rational Positive-Inexact-Real)]
+  [NonNegReal (Nonnegative-Rational Nonnegative-Inexact-Real)]
+  [NegReal (Negative-Rational Negative-Inexact-Real)]
+  [NonPosReal (Nonpositive-Rational Nonpositive-Inexact-Real)]
+  [Real (Rational Inexact-Real)]
+
+  [Exact-Number (Exact-Imaginary Exact-Complex Rational)]
+  [Inexact-Imaginary (Float-Imaginary Single-Float-Imaginary)]
+  [Imaginary (Exact-Imaginary Inexact-Imaginary)]
+  [Inexact-Complex (Float-Complex Single-Float-Complex)]
+  [Number (Real Imaginary Exact-Complex Inexact-Complex)])
+
+
+
+(define (index? x) (and (fixnum? x) (>= x 0) (fixnum? (* x 4))))
+
+(define (exact-rational? x) (and (rational? x) (exact? x)))
+
+(define nbase->pred
+  (let ([table
+         (hash
+          'Zero (λ (n) (eq? n 0))
+          'One (λ (n) (eq? n 1))
+          'Byte-Larger-Than-One (λ (n) (and (byte? n) (> n 1)))
+          'Positive-Index-Not-Byte (λ (x) (and (index? x)
+                                               (positive? x)
+                                               (not (byte? x))))
+          'Positive-Fixnum-Not-Index (λ (x) (and (fixnum? x)
+                                                 (positive? x)
+                                                 (not (index? x))))
+          'Negative-Fixnum (λ (x) (and (fixnum? x)
+                                       (negative? x)))
+          'Positive-Integer-Not-Fixnum (λ (x) (and (exact-integer? x)
+                                                   (positive? x)
+                                                   (not (fixnum? x))))
+          'Negative-Integer-Not-Fixnum (λ (x) (and (exact-integer? x)
+                                                   (negative? x)
+                                                   (not (fixnum? x))))
+          'Positive-Rational-Not-Integer (λ (x) (and (exact-rational? x)
+                                                     (positive? x)
+                                                     (not (exact-integer? x))))
+          'Negative-Rational-Not-Integer (λ (x) (and (exact-rational? x)
+                                                     (negative? x)
+                                                     (not (exact-integer? x))))
+          'Float-NaN (λ (x) (and (flonum? x) (eqv? x +nan.0)))
+          'Float-Positive-Zero (λ (x) (eqv? x 0.0))
+          'Float-Negative-Zero (λ (x) (eqv? x -0.0))
+          'Positive-Float-No-NaN (λ (x) (and (flonum? x) (positive? x)))
+          'Negative-Float-No-NaN (λ (x) (and (flonum? x) (negative? x)))
+          'Single-Flonum-NaN (λ (x) (and (single-flonum? x) (eqv? x +nan.f)))
+          'Single-Flonum-Positive-Zero (λ (x) (eqv? x 0.0f0))
+          'Single-Flonum-Negative-Zero (λ (x) (eqv? x -0.0f0))
+          'Positive-Single-Flonum-No-NaN (λ (x) (and (single-flonum? x) (positive? x)))
+          'Negative-Single-Flonum-No-NaN (λ (x) (and (single-flonum? x) (negative? x)))
+          'Exact-Imaginary (λ (x) (and (number? x)
+                                       (not (real? x))
+                                       (eqv? 0 (real-part x))
+                                       (exact? (imag-part x))))
+          'Exact-Complex (λ (x) (and (number? x)
+                                     (not (real? x))
+                                     (not (eqv? 0 (real-part x)))
+                                     (exact? (real-part x))
+                                     (exact? (imag-part x))))
+          'Float-Imaginary (λ (x)
+                             (and (number? x)
+                                  (flonum? (imag-part x))
+                                  (eqv? 0 (real-part x))))
+          'Single-Flonum-Imaginary (λ (x)
+                                     (and (number? x)
+                                          (single-flonum? (imag-part x))
+                                          (eqv? 0 (real-part x))))
+          'Float-Complex (λ (x)
+                           (and (number? x)
+                                (flonum? (imag-part x))
+                                (flonum? (real-part x))))
+          'Single-Flonum-Complex (λ (x)
+                                   (and (number? x)
+                                        (single-flonum? (imag-part x))
+                                        (single-flonum? (real-part x)))))])
+    (λ (nt) (hash-ref table nt
+                      (λ () (error 'nbase->pred "~v is not a known numeric base type" nt))))))
---
+++ syntactic-lang.rkt
@@ -0,0 +1,123 @@
+#lang racket/base
+
+(require redex/reduction-semantics
+         racket/match
+         racket/set
+         racket/list
+         "base-lang.rkt")
+
+(define-extended-language syntactic base
+  [btype ::= True False (Or True False) (Or False True)]
+  [type ::= ntype btype]
+  [uprop ::= tt (∈ 0 type) (∉ 0 type)]
+  [cprop ::= tt (∈ 0 type) (∈ 1 type) (∉ 0 type) (∉ 1 type) (∧ (∈ 0 type) (∈ 1 type))]
+  [uR ::= (Res type uprop uprop)]
+  [uarrow ::= (-> type R)]
+  [ucase ::= (ucase-> uarrow uarrow ...)]
+  [barrow ::= (-> type type type)]
+  [bcase ::= (bcase-> barrow barrow ...)]
+  [cR ::= (Res btype cprop cprop)]
+  [carrow ::= (-> type type cR)]
+  [ccase ::= (ccase-> carrow carrow ...)])
+
+;; roughly the intersection of two types
+(define-metafunction syntactic
+  restrict : ntype ntype -> ntype
+  [(restrict nbase_!_ nbase_!_) (Or)]
+  [(restrict nbase nbase) nbase]
+  [(restrict nbase (Or any_0 ... nbase any_1 ...)) nbase]
+  [(restrict nbase (Or any_0 ...)) (Or)]
+  [(restrict (Or any_0 ... nbase any_1 ...) nbase) nbase]
+  [(restrict (Or any_0 ...) nbase) (Or)]
+  [(restrict (Or nbase_0 ...) (Or nbase_1 ...))
+   (Or nbase_2 ...)
+   (where (nbase_2 ...) ,(set-intersect (term (nbase_0 ...)) (term (nbase_1 ...))))])
+
+
+;; roughly type subtraction
+(define-metafunction syntactic
+  remove : ntype ntype -> ntype
+  [(remove nbase nbase) (Or)]
+  [(remove nbase_1 nbase_2) nbase_1]
+  [(remove nbase_0 (Or nbase_1 ... nbase_0 nbase_2 ...)) (Or)]
+  [(remove nbase_0 (Or nbase_1 ...)) nbase_0]
+  [(remove (Or nbase_0 ...) nbase_1)
+   (Or nbase_2 ...)
+   (where (nbase_2 ...) ,(set-remove (term (nbase_0 ...)) (term nbase_1)))]
+  [(remove (Or nbase_0 ...) (Or nbase_1 ...))
+   (Or nbase_2 ...)
+   (where (nbase_2 ...) ,(set-subtract (term (nbase_0 ...)) (term (nbase_1 ...))))])
+
+
+;; simple subtyping
+(define-metafunction syntactic
+  nsubtype : ntype ntype -> boolean
+  [(nsubtype ntype ntype) #t]
+  [(nsubtype (Or nbase_1 ...) ntype)
+   ,(for/and ([nb (in-list (term (nbase_1 ...)))])
+      (term (nsubtype ,nb ntype)))]
+  [(nsubtype ntype (Or nbase_1 ...))
+   ,(for/or ([nb (in-list (term (nbase_1 ...)))])
+      (term (nsubtype ntype ,nb)))]
+  [(nsubtype _ _) #f])
+
+(define-metafunction syntactic
+  nequiv : ntype ntype -> boolean
+  [(nequiv ntype_1 ntype_2)
+   ,(and (term (nsubtype ntype_1 ntype_2))
+         (term (nsubtype ntype_2 ntype_1)))])
+
+(define (equiv-types t1 t2)
+  (term (nequiv ,t1 ,t2)))
+
+
+
+
+
+
+
+(module+ test
+  (require (for-syntax racket/base))
+  (define-syntax (test-nequiv stx)
+    (syntax-case stx ()
+      [(_ t1 t2)
+       (syntax/loc #'t1 (test-equal t1 t2 #:equiv equiv-types))]))
+
+  ;; subtyping tests
+  (test-equal (term (nsubtype Zero Zero)) #t)
+  (test-equal (term (nsubtype Zero One)) #f)
+  (test-equal (term (nsubtype Zero (Or Zero One))) #t)
+  (test-equal (term (nsubtype (Or Zero One) Zero)) #f)
+  (test-equal (term (nsubtype (Or Zero One) (Or One Zero))) #t)
+  (test-equal (term (nsubtype (Or Zero One) (Or One Zero Byte-Larger-Than-One))) #t)
+  (test-equal (term (nsubtype (Or One Zero Byte-Larger-Than-One) (Or Zero One))) #f)
+  
+  ;; restrict tests
+  (test-nequiv (term (restrict Zero Zero)) (term Zero))
+  (test-nequiv (term (restrict Zero One)) (term (Or)))
+  (test-nequiv (term (restrict (Or Zero One) One)) (term One))
+  (test-nequiv (term (restrict One (Or Zero One))) (term One))
+  (test-nequiv (term (restrict (Or Zero One) (Or Zero One))) (term (Or Zero One)))
+  (test-nequiv (term (restrict (Or Zero One Byte-Larger-Than-One)
+                               (Or Zero One)))
+               (term (Or Zero One)))
+  (test-nequiv (term (restrict (Or Zero One) (Or Zero One Byte-Larger-Than-One)))
+               (term (Or Zero One)))
+
+  ;; remove tests
+  (test-nequiv (term (remove Zero Zero)) (term (Or)))
+  (test-nequiv (term (remove Zero One)) (term Zero))
+  (test-nequiv (term (remove (Or Zero One) One)) (term Zero))
+  (test-nequiv (term (remove One (Or Zero One))) (term (Or)))
+  (test-nequiv (term (remove (Or Zero One) (Or Zero One))) (term (Or)))
+  (test-nequiv (term (remove (Or Zero One Byte-Larger-Than-One)
+                             (Or Zero One)))
+               (term Byte-Larger-Than-One))
+  (test-nequiv (term (remove (Or Zero One) (Or Zero One Byte-Larger-Than-One)))
+               (term (Or)))
+
+  (test-results))
+
+
+
+
