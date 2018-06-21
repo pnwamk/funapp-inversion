710ca5651ec10076b61145bb8274a619ac7b7a1c
Andrew Kent <pnwamk@gmail.com>
2018-06-21 11:51:20 -0400

    diff subset generator

--- haskell/src/Common/SetOps.hs
+++ haskell/src/Common/SetOps.hs
@@ -5,19 +5,33 @@ module Common.SetOps
   ) where
 
 import Data.List
+import Data.Bits
 
 -- powerset of a list
-subsets :: (Eq x) => [x] -> [[x]]
-subsets [] = [[]]
-subsets (x:xs) = (map (\ys -> x:ys) xss) ++ xss 
-  where xss = (subsets xs)
+subsets :: (Eq a) => [a] -> [[a]]
+subsets xs = combinations xs (\_ -> True)
 
 -- powerset of a list minus original list
 -- (i.e. all the strict subsets)
-strictSubsets :: (Eq x) => [x] -> [[x]]
-strictSubsets xs = delete xs (subsets xs)
-
+strictSubsets :: (Eq a) => [a] -> [[a]]
+strictSubsets xs = combinations xs (\n -> not (allIncluded == n))
+  where allIncluded = (2 ^ (length xs)) - 1
+    
 -- powerset of a list minus the empty list
 -- (i.e. all the strict subsets)
-nonEmptySubsets :: (Eq x) => [x] -> [[x]]
-nonEmptySubsets xs = delete [] (subsets xs)
+nonEmptySubsets :: (Eq a) => [a] -> [[a]]
+nonEmptySubsets xs = combinations xs (\n -> n > 0)
+
+
+combinations :: (Eq a) => [a] -> (Int -> Bool) -> [[a]]
+combinations xs include = aux 0
+  where maxSize = (2 ^ (length xs))
+        indexedData = zip xs [0..]
+        aux cur = if (cur < maxSize)
+                  then if (include cur)
+                       then (gen cur):rest
+                       else rest
+                  else []
+          where rest = aux (cur + 1)
+                gen n = [x | (x , idx) <- indexedData, testBit n idx]
+                     
