c5c6a838091cef0e0d95ab685af1e187ea21f2ba
Andrew Kent <pnwamk@gmail.com>
2018-08-25 23:33:11 -0400

    tidy up Parsec parser

--- haskell/src/Repl/Parser.hs
+++ haskell/src/Repl/Parser.hs
@@ -7,25 +7,14 @@ import Text.Parsec.Prim (parserZero, parserFail)
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
-parseCmd :: String -> Either String Cmd
-parseCmd = run_parser cmdParser
-
-cmdParser :: Parser Cmd 
-cmdParser =
-  try parseInhabited
-  <|> try parseSubtype
-  <|> try parseFstProj
-  <|> try parseSndProj
-  <|> try parseFunApp
-  <|> try parseFunInv
-  <|> try parseQuit
-  <|> try parseHelp
-  <|> parserFail "unrecognized command"
-
 run_parser :: Parser a -> String -> Either String a
 run_parser p str =  case parse p "" str of
   Left err -> Left $ "parse error at " ++ (show err)
   Right val  -> Right val  
+
+
+parseCmd :: String -> Either String Cmd
+parseCmd = run_parser cmdParser
 
 spaces :: Parser ()
 spaces = skipMany space
@@ -43,131 +32,103 @@ closeParen = do
   return ()
 
 
-parseInhabited :: Parser Cmd
-parseInhabited = do
+cmdParser :: Parser Cmd 
+cmdParser = do
   openParen
-  string "Inhabited"
-  spaces
-  [t] <- parseTyList
-  closeParen
-  return $ Inhabited t
-
-
-parseSubtype :: Parser Cmd
-parseSubtype = do
-  openParen
-  string "Subtype"
-  spaces
-  [t1,t2] <- parseTyList
-  char ')'
-  return $ Subtype t1 t2
-
-
-parseFstProj :: Parser Cmd
-parseFstProj = do
-  openParen
-  string "FstProj"
-  spaces
-  [t] <- parseTyList
-  closeParen
-  return $ FstProj t
-
-parseSndProj :: Parser Cmd
-parseSndProj = do
-  openParen
-  string "SndProj"
-  spaces
-  [t] <- parseTyList
-  closeParen
-  return $ SndProj t
-
-parseFunApp :: Parser Cmd
-parseFunApp = do
-  openParen
-  string "FunApp"
-  spaces
-  [t1,t2] <- parseTyList
-  closeParen
-  return $ FunApp t1 t2
-
-parseFunInv :: Parser Cmd
-parseFunInv = do
-  openParen
-  string "FunInv"
-  spaces
-  [t1,t2,t3] <- parseTyList
-  closeParen
-  return $ FunInv t1 t2 t3
-
-parseQuit :: Parser Cmd
-parseQuit = do
-  openParen
-  string "Quit"
-  closeParen
-  return Quit
-
-parseHelp :: Parser Cmd
-parseHelp = do
-  char '('
-  string "Help"
-  char ')'
-  return Help
-
+  cmd <- many letter
+  case cmd of
+    "Inhabited" -> do
+      spaces
+      t <- parseTy
+      closeParen
+      return $ Inhabited t
+    "Subtype" -> do
+      spaces
+      t1 <- parseTy
+      spaces
+      t2 <- parseTy
+      closeParen
+      return $ Subtype t1 t2
+    "Project" -> do
+      i <- char '1' <|> char '2' <?> "a 1 or 2"
+      spaces
+      t <- parseTy
+      closeParen
+      case i of
+        '1' -> return $ FstProj t
+        '2' -> return $ SndProj t
+    "Apply" -> do
+      spaces
+      t1 <- parseTy
+      spaces
+      t2 <- parseTy
+      closeParen
+      return $ FunApp t1 t2
+    "Inversion" -> do
+      spaces
+      t1 <- parseTy
+      spaces
+      t2 <- parseTy
+      spaces
+      t3 <- parseTy
+      closeParen
+      return $ FunInv t1 t2 t3
+    "Quit" -> do
+      closeParen
+      return Quit
+    "Help" -> do
+      closeParen
+      return Help
+    _ -> parserFail $ "'" ++ cmd ++ "' is not a valid command!"
 
 parseTy :: Parser BDD.Ty
-parseTy = parseTyName <|> do openParen
-                             t <- parseCompoundTy
-                             closeParen
-                             return t
+parseTy = parseTyName <|> parseCompoundTy
 
 parseTyName :: Parser BDD.Ty
-parseTyName = do 
+parseTyName = do
   name <- many letter
   case (nameToTy name) of
     Just t -> return t
-    Nothing -> parserZero
+    Nothing -> parserFail $ "'" ++ name ++ "' is not a valid type name!"
+
+parseTyList :: Parser [BDD.Ty]
+parseTyList = do
+  spaces
+  t <- parseTy
+  sepBy parseTy space
+
 
 parseCompoundTy :: Parser BDD.Ty
 parseCompoundTy = do
-  t <- parseOr <|> parseAnd <|> parseNot <|> parseArrow <|> parseProd
-  return t
+  openParen
+  constructor <- many letter
+  case constructor of
+    "Or" -> do
+      ts <- parseTyList
+      closeParen
+      return $ BDD.tyOr' ts
+    "And" -> do
+      ts <- parseTyList
+      closeParen
+      return $ BDD.tyAnd' ts
+    "Not" -> do
+      spaces
+      t <- parseTy
+      closeParen
+      return $ BDD.tyNot t
+    "Arrow" -> do
+      spaces
+      t1 <- parseTy
+      spaces
+      t2 <- parseTy
+      closeParen
+      return $ BDD.arrowTy t1 t2
+    "Prod" -> do
+      spaces
+      t1 <- parseTy
+      spaces
+      t2 <- parseTy
+      closeParen
+      return $ BDD.prodTy t1 t2
+    _ -> parserFail $ "'" ++ constructor ++ "' is not a valid type constructor"
   
-
-parseTyList :: Parser [BDD.Ty]
-parseTyList = sepBy parseTy space
-
-
-parseOr :: Parser BDD.Ty
-parseOr = do
-  string "Or"
-  spaces
-  ts <- parseTyList
-  return $ BDD.tyOr' ts
-
-parseAnd :: Parser BDD.Ty
-parseAnd = do
-  string "And"
-  spaces
-  ts <- parseTyList
-  return $ BDD.tyAnd' ts
-  
-parseNot :: Parser BDD.Ty
-parseNot = do
-  string "Not"
-  spaces
-  [t] <- parseTyList
-  return $ BDD.tyNot t
-
-parseArrow :: Parser BDD.Ty
-parseArrow = do
-  string "Fun"
-  spaces
-  [t1,t2] <- parseTyList
-  return $ BDD.arrowTy t1 t2
-
-parseProd :: Parser BDD.Ty
-parseProd = do
-  string "Pair"
-  spaces
-  [t1,t2] <- parseTyList
-  return $ BDD.prodTy t1 t2
