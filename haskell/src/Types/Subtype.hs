c90aade0f1de809106599da561f6df294e53ecb3
Andrew Kent <pnwamk@gmail.com>
2018-09-06 10:00:05 -0400

    in progress seen in subtyping

--- haskell/src/Types/Subtype.hs
+++ haskell/src/Types/Subtype.hs
@@ -5,36 +5,53 @@ module Types.Subtype
   , isEmpty
   ) where
 
-import Types.LazyBDD
+import           Types.LazyBDD
+import           Data.Set (Set)
+import qualified Data.Set as Set
 
 -- Is this type equivalent to ∅?
 isEmpty :: Ty -> Bool
-isEmpty (Ty b ps as) = mtBase && mtProds && mtArrows
-  where mtBase   = b == emptyBase
-        mtProds  = isEmptyProd ps anyTy anyTy []
-        mtArrows = isEmptyArrow as emptyTy [] []
+isEmpty t = case mtTy Set.empty t of
+              Just _  -> True
+              Nothing -> False
 
+type Seen = Set FiniteTy
 
+-- internal isEmpty which tracks seen types
+mtTy :: Ty -> Seen -> Maybe Seen
+mtTy (Ty (Base True 0) ps as) seen =
+  isEmptyProd ps anyTy anyTy [] seen
+  >>=  isEmptyArrow as emptyTy [] []
+mtTy seen (Ty _ _ _) = Nothing
+mtTy seen t@(TyNode fty b ps as)
+  | Set.member seen fty  = Just seen
+  | not $ b == emptyBase = Nothing
+  | otherise = 
+      isEmptyProd ps anyTy anyTy [] seen
+      >>= isEmptyArrow as emptyTy [] []
+  
+
+-- BOOKMARK, making this monadic in Maybe Seen
 -- Is a BDD of prods equivalent to ∅?
-isEmptyProd :: (BDD Prod) -> Ty -> Ty -> [Prod] -> Bool
-isEmptyProd (Node p@(Prod t1 t2) l m r) s1 s2 neg =
-  (isEmptyProd l (tyAnd s1 t1) (tyAnd s2 t2) neg)
-  && (isEmptyProd m s1 s2 neg)
-  && (isEmptyProd r s1 s2 (p:neg))
-isEmptyProd Bot _ _ _ = True
-isEmptyProd Top s1 s2 neg = ((isEmpty s1)
-                              || (isEmpty s2)
-                              || (go s1 s2 neg))
+isEmptyProd :: (BDD Prod) -> Ty -> Ty -> [Prod] -> Seen -> Maybe Seen
+isEmptyProd (Node p@(Prod t1 t2) l m r) s1 s2 neg seen =
+  isEmptyProd l (tyAnd s1 t1) (tyAnd s2 t2) neg seen
+  >>= isEmptyProd seen m s1 s2 neg
+  >>= isEmptyProd seen r s1 s2 (p:neg)
+isEmptyProd Bot _ _ _ _ = True
+isEmptyProd Top s1 s2 neg seen = isEmpty seen s1
+                                 <|> isEmpty seen s2
+                                 <|> go s1 s2 neg
   where go :: Ty -> Ty -> [Prod] -> Bool
         go s1 s2 [] = False
         go s1 s2 ((Prod t1 t2):neg) = lEmpty && rEmpty
-          where lEmpty = ((isEmpty diff1) || (go diff1 s2 neg))
-                diff1 = (tyDiff s1 t1)
-                rEmpty = ((isEmpty diff2) || (go s1 diff2 neg))
-                diff2 = (tyDiff s2 t2)
+          where lEmpty = (isEmpty diff1) <|> (go diff1 s2 neg)
+                diff1 = tyDiff s1 t1
+                rEmpty = ((isEmpty diff2) <|> (go s1 diff2 neg))
+                diff2 = tyDiff s2 t2
 
 -- Is a BDD of arrows equivalent to ∅?
-isEmptyArrow :: (BDD Arrow) -> Ty -> [Arrow] -> [Arrow] -> Bool
+isEmptyArrow :: Seen -> (BDD Arrow) -> Ty -> [Arrow] -> [Arrow] -> Maybe Seen
 isEmptyArrow (Node a@(Arrow s1 s2) l m r) dom pos neg =
   (isEmptyArrow l (tyOr s1 dom) (a:pos) neg)
   && (isEmptyArrow m dom pos neg)
